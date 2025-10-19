f4758c44a401f44e6ba2c0b8a9144ede54d7f4e4
IWKMS99 <alievsaid0909@yandex.ru>
2025-10-19 20:43:39 +0300

    feat(security): добавлена конфигурация безопасности с настройками для API и Swagger

---
+++ src/main/java/iwkms/roomflow/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package iwkms.roomflow.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/api/v1/**",
+                                "/actuator/**",
+                                "/swagger-ui.html",
+                                "/swagger-ui/**",
+                                "/v3/api-docs/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                )
+
+                .sessionManagement(
+                        session -> session.sessionCreationPolicy(STATELESS)
+                );
+
+        return http.build();
+    }
+}
