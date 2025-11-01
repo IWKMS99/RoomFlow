7e4a9e70ec2545753d5940fa8f978a4a6984b441
IWKMS99 <alievsaid0909@yandex.ru>
2025-11-02 00:29:19 +0300

    feat(dto): добавлен конструктор с проверкой на null и переопределен метод rooms в TimeSlotViewDto

--- src/main/java/iwkms/roomflow/modules/booking/api/dto/TimeSlotViewDto.java
+++ src/main/java/iwkms/roomflow/modules/booking/api/dto/TimeSlotViewDto.java
@@ -3,4 +3,14 @@ package iwkms.roomflow.modules.booking.api.dto;
 import java.time.LocalTime;
 import java.util.List;
 
-public record TimeSlotViewDto(LocalTime time, List<RoomInScheduleViewDto> rooms) {}
+public record TimeSlotViewDto(LocalTime time, List<RoomInScheduleViewDto> rooms) {
+    public TimeSlotViewDto(LocalTime time, List<RoomInScheduleViewDto> rooms) {
+        this.time = time;
+        this.rooms = rooms != null ? List.copyOf(rooms) : List.of();
+    }
+
+    @Override
+    public List<RoomInScheduleViewDto> rooms() {
+        return rooms;
+    }
+}
